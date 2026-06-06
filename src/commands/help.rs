48f1fccb87a19153a320f1c9ca6589661bbd7bcf
SECR1T ATHAN <altairsecrit@gmail.com>
2026-06-06 20:31:35 +0000

    Parser of errors/logs

---
+++ src/commands/help.rs
@@ -0,0 +1,11 @@
+pub fn run(_args: &[String]){
+println!(r#"
+        Welcome to RootLine
+
+Commands:
+  git push 
+  git commit
+  git new
+"#);
+}
+
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,7 @@
 
 pub mod ip;
 pub mod git;
+pub mod help;
 
 pub fn run(args: &[String]) {
     let cmd = &args[0];
@@ -8,6 +9,7 @@ pub fn run(args: &[String]) {
     match cmd.as_str() {
         "ip" => ip::run(&args[1..]),
         "git" => git::run(&args[1..]),
+        "help" => help::run(&args[1..]),
         _ => println!("Unknown command: {}", cmd),
     }
 }
